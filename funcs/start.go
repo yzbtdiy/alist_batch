d11e836d5e50a9e159e2b7d395f8519017dee061
yzbtdiy <admin@opensource.cool>
2023-01-12 11:27:29 +0800

    add some panics

--- funcs/start.go
+++ funcs/start.go
@@ -82,21 +82,18 @@ func Start() {
 		//token无效重新获取
 		UpdateToken(loginApi, authJson, conf)
 	}
-
 }
 
+// 检查配置文件是否修改
 func CheckConf(conf *models.Config) {
-
 	if conf.Url == "ALIST_URL" {
-		log.Println("url 未配置, 请检查配置文件")
-
+		panic("url 未配置, 请检查配置文件")
+	}
+	if (conf.Auth.Username == "USERNAME" || conf.Auth.Password == "PASSWORD") && (conf.Token == "ALIST_TOKEN" || conf.Token == "") {
+		panic("token和用户密码至少要配置一项, 请检查配置文件")
 	}
 	if conf.RefreshToken == "ALI_YUNPAN_REFRESH_TOKEN" {
-		log.Println("refresh_token 未配置, 请检查配置文件")
-	}
-
-	if conf.Auth.Username == "USERNAME" && conf.Auth.Password == "PASSWORD" && conf.Token == "ALIST_TOKEN" {
-		log.Println("token和用户密码至少要配置一项, 请检查配置文件")
+		panic("refresh_token 未配置, 请检查配置文件")
 	}
 }
 
@@ -109,6 +106,8 @@ func UpdateToken(loginApi string, authJson []byte, conf *models.Config) {
 	// log.Println(tokenData.Token)
 	if resData.Code == 200 {
 		ModConfig("config.yaml", conf, tokenData.Token)
-		log.Println("token已更新, 请重新运行此程序")
+		log.Println("token 已更新, 请重新运行此程序")
+	} else {
+		panic("token 更新失败, 请检查用户密码是否正确")
 	}
 }
